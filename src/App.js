db7f9a2c84ecdccb558c0ed999ed5d37feef3a19
unknown <jvas1999@gmail.com>
2021-10-21 10:21:51 -0300

    up

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section>
+      <h1>Aprendendo React</h1>
+      <img src="https://casadospassaros.net/wp-content/uploads/2016/12/Coruja-Buraqueira-1200x900.jpg" alt="coruja"></img>
+      
+      </section>
+      
+      <section>
+      <h1>Por que a coruja é símbolo de sabedoria?</h1>
+      
+      <pre>Por influência da mitologia grega, tanto que Atena, deusa da guerra e da sabedoria, tinha uma coruja como mascote.
+            Os gregos consideravam a noite como o momento do pensamento filosófico e da revelação intelectual e a coruja
+            por ser uma ave noturna, acabou representando essa busca pelo saber.
+            Há ainda uma outra explicação para tal relação, da qual, certamente
+            o animal não se orgulharia tanto. Com seus olhos grandes e desproporcionais
+            a coruja se tornou também símbolo da feiúra.Abaixo fotos de como a coruja era retratada na mitologia:
+      </pre>
+      </section>
+      </div>
+})
+export default App;
